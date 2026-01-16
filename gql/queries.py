468f203eb9dacdf5038048b9fb9a56b72f66ad79
Tanujcbe <tanujcbe@gmail.com>
2026-01-17 00:55:47 +0530

    feat: Add User GraphQL type and implement queries for fetching all users and a single user by ID.

--- gql/queries.py
+++ gql/queries.py
@@ -1,9 +1,17 @@
 import strawberry
 from mock_db import users
+from gql.types import User
 
 @strawberry.type
 class Query:
     @strawberry.field
     def hello_world(self)->str:
-        print([user["name"] for user in users])
         return "Hellow World"
+    
+    @strawberry.field
+    def users(self)->list[User]:
+        return [User(**user)  for user in users]
+    
+    @strawberry.field
+    def user(self, id:int)->User:
+        return User(**users[id-1])
